6259cca5b55af9c1952d1b1da0536dcd2023b75b
grsjob <92042203+grsjob@users.noreply.github.com>
2023-04-07 08:51:12 +0300

    Update Check.tsx

--- src/components/screens/Home/item/Check.tsx
+++ src/components/screens/Home/item/Check.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BsCheck} from 'react-icons/bs'
-
+//опять же - проще CheckProps
 type State = {
     isCompleted: boolean
 }
 
 const Check = ({isCompleted}: State) => {
     return (
+        //юзай библиотеку classnames смотрится как абракадабра в которой еще и внутри строки какие то проверки идут. Такое нельзя держать в разметке))
+        //либа позволить сконфигурировать гибко константу и уже ее одну отправить в поле className
         <div className={`flex items-center 
             justify-center border-2 rounded-lg border-pink-400 
             ${isCompleted ? 'bg-pink-400' : ''} 
