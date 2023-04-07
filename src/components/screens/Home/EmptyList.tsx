660f4e4e2056af592e33e542166e37eb8a3f6c0c
grsjob <92042203+grsjob@users.noreply.github.com>
2023-04-07 08:38:26 +0300

    Update EmptyList.tsx

--- src/components/screens/Home/EmptyList.tsx
+++ src/components/screens/Home/EmptyList.tsx
@@ -5,6 +5,7 @@ const EmptyList = () => {
     return (
         <div className="flex items-center text-center justify-center">
             <p className="text-center text-gray-400 transform text-xl mr-2">Список пуст</p>
+            //"магические числа" лучше выносить в константы иначе когда не видно единиц измерения хрен поймешь почему 22)) 
             <FaScroll size={22} className="text-gray-400"/>
         </div>
     )
