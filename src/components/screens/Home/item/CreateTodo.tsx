18beb286e92b9c2dfc5e49ad0364ee6d297bffe4
grsjob <92042203+grsjob@users.noreply.github.com>
2023-04-07 08:55:50 +0300

    Update CreateTodo.tsx

--- src/components/screens/Home/item/CreateTodo.tsx
+++ src/components/screens/Home/item/CreateTodo.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react"
 import { VscAdd } from "react-icons/vsc"
-
+//React.Dispatch<React.SetStateAction<string>> вот такое лучше не передавать. К тому же ты как то слишком сложно завернул. Функция, которая аргументом принимает 
+//реактовский диспатч и это еще и в пропсах все. Какая то жесть)) разбей отдельно. СТейтовые функции должны использоваться в этом же компоненте, либо получать
+//обертку и передаваться дальше по дереву пропсами, завернутыми в юз колбек. а тут пропс, который нельзя менять, принимает диспатч реакта. Жесть кака то)))
+//если тебе что то надо передать на уровень выше - напиши кастомный хук. используй контекст. Используй редакс. Либо пробрось отдельную функцию, котора обернет этот
+//диспатч, но не нужно это внутри пропса аргументом передавать)
 type AddTodo = {
     addTodo: (title: string, setTitle: React.Dispatch<React.SetStateAction<string>>) => void
 }
@@ -28,6 +32,7 @@ const CreateTodoField = ({addTodo}: AddTodo) => {
                 onKeyDown={onKeyDown}  
                 placeholder="Введите задачу"  
             />
+            //вот такого лучше избегать. не очевидно с первого взгляда что это булевое. преобразуй сверху нормально и сюда вставь константу
             {!!title.length &&
                 <button className="pl-4" onClick={() => addTodo(title, setTitle)}>
                     <VscAdd size={22} className='text-gray-300 hover:text-green-600 transition-colors ease-in-out duration-300'/>
